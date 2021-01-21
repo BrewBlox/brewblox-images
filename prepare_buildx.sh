08a33ec734f76bf9c296d5bba94bc9b079ed8ae1
Bob Steers <steers.bob@gmail.com>
2021-01-21 13:54:31 +0100

    update buildx version

--- prepare_buildx.sh
+++ prepare_buildx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-BUILDX_URL=https://github.com/docker/buildx/releases/download/v0.4.1/buildx-v0.4.1.linux-amd64
+BUILDX_URL=https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-amd64
 
 curl -L -o ~/.docker/cli-plugins/docker-buildx --create-dirs ${BUILDX_URL}
 chmod a+x ~/.docker/cli-plugins/docker-buildx
