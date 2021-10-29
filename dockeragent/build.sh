ca84b7f99eb825e8914ec3006ac675e1c8b5f304
Bob Steers <steers.bob@gmail.com>
2021-10-29 15:17:50 +0200

    add scripts for manual dockeragent builds

---
+++ dockeragent/build.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+set -euo pipefail
+pushd "$(dirname "$0")" > /dev/null
+
+bash ../prepare_buildx.sh
+
+TAG="${1:?}"
+shift
+
+# don't forget to call with --push
+docker buildx build \
+    --pull \
+    --tag brewblox/dockeragent:"$TAG" \
+    --platform linux/arm/v7,linux/arm64/v8 \
+    "$@" \
+    .
