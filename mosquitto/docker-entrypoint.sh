1b539c0bc129bb177405d89237368ea070701867
Bob Steers <steers.bob@gmail.com>
2023-04-05 13:52:25 +0200

    override entrypoint script to touch passwd file

---
+++ mosquitto/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+set -e
+
+# Create files expected in mosquitto.conf
+mkdir -p /mosquitto/include
+touch /mosquitto/include/externals.passwd
+
+# Set permissions
+user="$(id -u)"
+if [ "$user" = '0' ]; then
+    chown -R mosquitto:mosquitto /mosquitto || true
+fi
+
+exec "$@"
